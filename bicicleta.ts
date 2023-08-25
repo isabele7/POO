350f0f813873070365fe83e89c25e7025c84e370
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-08-25 11:32:47 -0300

    Update bicicleta.ts

--- bicicleta.ts
+++ bicicleta.ts
@@ -1,1 +1,14 @@
-export class
+export class Bicicleta {
+    codigo: string;
+    marca: string;
+    preco: number;
+    disponivel: boolean;
+    usuarioAtual: string | null = null;
+
+    constructor(codigo: string, preco: number, disponivel: boolean, marca: string) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.preco = preco;
+        this.disponivel = disponivel;
+    }
+}
