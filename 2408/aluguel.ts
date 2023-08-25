09d29ec6349b73cc4e48468c5dd1d62d0e9d7fdc
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-08-25 11:40:30 -0300

    Create aluguel.ts

---
+++ 2408/aluguel.ts
@@ -0,0 +1,9 @@
+export class Aluguel {
+    dataInicio: Date;
+    dataTermino: Date;
+
+    constructor(dataInicio: Date, dataTermino: Date) {
+        this.dataInicio = dataInicio;
+        this.dataTermino = dataTermino;
+    }
+}
