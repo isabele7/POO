72dd668286b92ebbbde5e8ca87badb137b92b888
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-08-25 11:38:17 -0300

    Update cliente.ts

--- 2408/cliente.ts
+++ 2408/cliente.ts
@@ -1,1 +1,27 @@
+import { Aluguel } from "./aluguel";
+import { Bicicleta } from "./bicicleta";
 
+export class Usuario {
+    nome: string;
+    dinheiro: number;
+
+    constructor(nome: string, dinheiro: number) {
+        this.nome = nome;
+        this.dinheiro = dinheiro;
+    }
+
+    alugar(bicicleta: Bicicleta, dataInicio: Date, dataTermino: Date){
+        if (bicicleta.disponivel && bicicleta.usuarioAtual === null) {
+            const aluguel = new Aluguel(dataInicio, dataTermino); 
+            bicicleta.usuarioAtual = this.nome;
+            bicicleta.disponivel = false;
+            return aluguel;
+        }
+    }
+
+    pagar(preco: number): void {
+        if (this.dinheiro >= preco) {
+            this.dinheiro -= preco;
+        }
+    }
+}
