2c8bda21e622a4570d5d7a76dd97404b1ed30553
isabele7 <104145203+isabele7@users.noreply.github.com>
2023-08-26 12:33:17 -0300

    Update cliente.ts

--- 2408/cliente.ts
+++ 2408/cliente.ts
@@ -1,5 +1,5 @@
-import { Aluguel } from "./aluguel";
 import { Bicicleta } from "./bicicleta";
+import { Aluguel } from "./aluguel";
 
 export class Usuario {
     nome: string;
@@ -10,19 +10,25 @@ export class Usuario {
         this.dinheiro = dinheiro;
     }
 
-    alugar(bicicleta: Bicicleta, dataInicio: Date, dataTermino: Date){
-        if (bicicleta.disponivel && bicicleta.usuarioAtual === null && this.dinheiro >= preco) {
-            const aluguel = new Aluguel(dataInicio, dataTermino); 
+    alugar(bicicleta: Bicicleta, dataInicio: Date, dataTermino: Date): Aluguel | null {
+        if (bicicleta.disponivel && bicicleta.usuarioAtual === null && this.dinheiro >= bicicleta.preco) {
+            const aluguel = new Aluguel(dataInicio, dataTermino);
+            aluguel.bicicleta = bicicleta;
+            aluguel.usuario = this;
             bicicleta.usuarioAtual = this.nome;
             bicicleta.disponivel = false;
-            this.dinheiro -= preco;
+            this.dinheiro -= bicicleta.preco;
             return aluguel;
         }
     }
 
-    pagar(preco: number): void {
-        if (this.dinheiro >= preco) {
-            this.dinheiro -= preco;
+    devolver(bicicleta: Bicicleta): void {
+        if (!bicicleta.disponivel && bicicleta.usuarioAtual === this.nome) {
+            bicicleta.usuarioAtual = null;
+            bicicleta.disponivel = true;
+            console.log("Bicicleta devolvida com sucesso.");
+        } else {
+            console.log("Você não pode devolver esta bicicleta.");
         }
     }
 }
