97c2eb4835ebe00ab9babd2f5cef7d9e05abc280
Isabele de Lima Nascimento <104145203+isabele7@users.noreply.github.com>
2023-09-16 19:28:33 -0300

    Update and rename bicicleta.ts to bike.ts

---
+++ 2408/bike.ts
@@ -0,0 +1,42 @@
+class Address {
+    constructor(
+      public street: string,
+      public neighborhood: string,
+      public city: string,
+      public state: string,
+      public postcode: string
+    ) {}
+  }
+  
+export class Bike {
+
+    constructor(
+      public name: string,
+      public type: string,
+      public bodySize: number,
+      public maxLoad: number,
+      public rate: number,
+      public description: string,
+      public ratings: number,
+      public address: Address,
+      public imageUrls: string[],
+      public available: boolean = true,
+      public id?: string
+    ) {}
+  
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+          },
+          (error) => {
+            throw new Error("Bike location unavailable.");
+          }
+        );
+      } else {
+        throw new Error("Bike location unavailable.");
+      }
+    }
+  }  
